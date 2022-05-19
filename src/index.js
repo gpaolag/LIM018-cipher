74e73ab6c5e39e5eb247e3f2447666b6a54709f9
Paola <glenypaola.7@gmail.com>
2022-05-19 12:22:58 -0500

    Funciones cifrado y descifrado completas y corriendo en la web, parte del diseño realizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import cipher from './cipher.js';
-console.log(cipher);
+
+
+
+const btndecifrar= document.getElementById("btndecifrar");
+btndecifrar.addEventListener("click",decifrar);
+
+const btncifrar= document.getElementById("btncifrar");
+btncifrar.addEventListener("click",cifrar);
+
+function cifrar(){
+  const wordc= document.getElementById('words').value;
+  const offset= document.getElementById('position').value;
+  document.getElementById("resp").innerHTML = cipher.encode(offset,wordc);
+}
+
+function decifrar(){
+  const wordde= document.getElementById('words').value;
+  const offsetde= document.getElementById('position').value;
+  document.getElementById("resp").innerHTML = cipher.decode(offsetde,wordde);
+}
